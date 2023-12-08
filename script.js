d12c3d7d48ed9a08795bc1b1725a9c35dfbe9cfe
EliTimeless <EliTimeless@users.noreply.github.com>
2023-12-08 18:12:04 +0100

    change js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function getValue(response) {
-  // Vytvoření pole objektů pro usnadnění práce se záznamy
   const results = [];
   for (let i = 0; i < 10; i++) {
     results.push({
@@ -8,7 +7,6 @@ function getValue(response) {
     });
   }
 
-  // Pole s ID příslušných HTML prvků
   const resultElements = [
     "result1",
     "result2",
@@ -22,14 +20,17 @@ function getValue(response) {
     "result10",
   ];
 
-  // Použití forEach smyčky pro aktualizaci HTML prvků
   resultElements.forEach((elementID, index) => {
     const resultElement = document.querySelector(`#${elementID}`);
     const { title, link } = results[index];
     resultElement.innerHTML = `${title}, <a href="${link}" target="_blank">${link}</a>`;
   });
+
+  // Přidání tlačítka pro uložení
+  const saveButton = document.getElementById("button");
+  saveButton.addEventListener("click", () => saveResultsToFile(results));
+  document.querySelector(".search-engine").appendChild(saveButton);
 }
-// resultElement.innerHTML = `${title}</a>`;
 
 function submitSearch(event) {
   event.preventDefault();
@@ -41,64 +42,36 @@ function searchGoogle(keywords) {
   let apiUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAS24ObvHc0y1rHtXTyu_59-kfA9MiaQCo&cx=66cd9a7c8106040bb&q=${keywords}`;
   axios.get(apiUrl).then(getValue);
 }
-let form = document.querySelector("#searchForm");
-form.addEventListener("submit", submitSearch);
 
-/*
-function exportToWord() {
-  // Získání obsahu, který chcete vložit do dokumentu Word
-  var content = document.getElementById("content").innerHTML;
+function saveResultsToFile(results) {
+  if (results.length > 0) {
+    // Vytvoření JSON souboru s výsledky
+    const jsonData = JSON.stringify(results, null, 2);
 
-  // Vytvoření šablony s použitím docxtemplater
-  var doc = new Docxtemplater();
-  doc.loadZip(new JSZip(docxtemplater(templaterSettings)));
+    // Vytvoření Blobu a otevření dialogu pro stažení souboru
+    const blob = new Blob([jsonData], {
+      type: "application/json;charset=utf-8",
+    });
+    const fileName = "vyhledane_vyrazy.json";
 
-  // Nahrazení šablonovatelných míst v dokumentu obsahem
-  doc.setData({
-    content: content,
-  });
-
-  // Generování dokumentu
-  doc.render();
-
-  // Uložení dokumentu ve formátu Word
-  var out = doc.getZip().generate({
-    type: "blob",
-    mimeType:
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  });
-
-  // Stáhnutí souboru
-  saveAs(out, "document.docx");
+    // Použití FileSaver.js pro stažení souboru
+    saveAs(blob, fileName);
+  }
 }
-/*
-        <button type="button" onclick="searchGoogle()">Vyhledat</button>
 
- <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-md-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-md-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
+document.addEventListener("DOMContentLoaded", function () {
+  let searchInput = document.querySelector("#input-keywords");
 
+  // Nastavení výchozího hledaného slova
+  let defaultKeyword = "collabim";
+  searchInput.value = defaultKeyword;
 
-            <div id="content">
-        <h1>Hello, World!</h1>
-        <p>This is the content you want to export to Word.</p>
-      </div>
+  // Spuštění vyhledávání po načtení stránky s výchozím slovem
+  searchGoogle(defaultKeyword);
 
-      <button onclick="exportToWord()">Export to Word</button>
-
-*/
+  // Přidání posluchače pro změnu vstupu uživatele
+  searchInput.addEventListener("input", function () {
+    // Spuštění vyhledávání po každé změně vstupu
+    searchGoogle(searchInput.value);
+  });
+});
